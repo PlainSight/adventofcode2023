98c16f264aae74d0a971bde033e490aed1668beb
Alex <alexander.ogier@gmail.com>
2023-12-01 18:35:52 +1300

    day1

--- day1/part1.js
+++ day1/part1.js
@@ -1,3 +1,17 @@
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var sum = 0;
+
+input.forEach(l => {
+    var digits = [];
+    l.split('').forEach(c => {
+        if(/\d/.test(c)) {
+            digits.push((c));
+        }
+    })
+    sum += parseInt(digits[0] + digits[digits.length-1]);
+});
+
+console.log(sum);
--- day1/part2.js
+++ day1/part2.js
@@ -1,3 +1,32 @@
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var sum = 0;
+
+var place = [
+    'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+];
+
+input.forEach(l => {
+    var digits = [];
+    var str = l;
+    while(str.length > 0) {
+        var found = /(\d|one|two|three|four|five|six|seven|eight|nine)/.exec(str);
+        if (found) {
+            str = str.substring(found.index + 1);
+            var fi = place.indexOf(found[0]);
+            if(fi > -1) {
+                digits.push(''+(fi+1));
+            } else {
+                digits.push(found[0])
+            }
+        } else {
+            break;
+        }
+    }
+    sum += parseInt(digits[0] + digits[digits.length-1]); 
+    //console.log(l, digits, parseInt(digits[0] + digits[digits.length-1]));
+});
+
+console.log(sum);
