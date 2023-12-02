548e174c8272a4159fa3549b82b4621824dfa1e2
Alex <alexander.ogier@gmail.com>
2023-12-02 21:13:26 +1300

    day2

--- day2/part1.js
+++ day2/part1.js
@@ -1,3 +1,30 @@
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var sumIds = 0;
+
+input.forEach(i => {
+    var vals = /Game (\d+): (.+)/.exec(i);
+    var gameId = parseInt(vals[1]);
+    var results = vals[2];
+    var sets = results.split(';').reduce((a, s) => {
+        s.trim().split(',').forEach(c => {
+            var bits = /(\d+) ([a-z]+)/.exec(c);
+            var col = bits[2];
+            var b = parseInt(bits[1]);
+            if ((a[col] || 0) < b) {
+                a[col] = b;
+            }
+        });
+        return a;
+    }, {});
+
+    if (sets['red'] <= 12 && sets['green'] <= 13 && sets['blue'] <= 14) {
+        sumIds += gameId;
+    }
+    
+    //console.log(gameId, sets);
+})
+
+console.log(sumIds);
--- day2/part2.js
+++ day2/part2.js
@@ -1,3 +1,28 @@
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var sumIds = 0;
+
+input.forEach(i => {
+    var vals = /Game (\d+): (.+)/.exec(i);
+    var gameId = parseInt(vals[1]);
+    var results = vals[2];
+    var sets = results.split(';').reduce((a, s) => {
+        s.trim().split(',').forEach(c => {
+            var bits = /(\d+) ([a-z]+)/.exec(c);
+            var col = bits[2];
+            var b = parseInt(bits[1]);
+            if ((a[col] || 0) < b) {
+                a[col] = b;
+            }
+        });
+        return a;
+    }, {});
+
+    var power = sets['red'] * sets['green'] * sets['blue'];
+
+    sumIds += power;
+})
+
+console.log(sumIds);
