0641127de5ac3afc4c155adec8afa5eecb5de7a7
Alex <alexander.ogier@gmail.com>
2023-12-05 00:57:17 +1300

    day4

--- day4/part1.js
+++ day4/part1.js
@@ -1,3 +1,24 @@
+const { match } = require('assert');
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var points = 0;
+
+input.forEach(i => {
+    var bits = /Card\s+(\d+):(.+)\|(.+)/.exec(i);
+    var winning = Object.values(bits[2].trim().split(' ').filter(f => f != '').reduce((a, c) => { a[c.trim()] = c.trim(); return a; }, {}));
+    var chances = Object.values(bits[3].trim().split(' ').filter(f => f != '').reduce((a, c) => { a[c.trim()] = c.trim(); return a; }, {}));
+    console.log('w', winning, 'c', chances);
+    var matches = 0;
+    chances.forEach(c => {
+        if (winning.includes(c)) {
+            matches++;
+        }
+    })
+    if (matches > 0) {
+        points += Math.pow(2, matches-1);
+    }
+})
+
+console.log(points);
--- day4/part2.js
+++ day4/part2.js
@@ -1,3 +1,50 @@
+const { match } = require('assert');
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var cards = {};
+
+function scoreCard(cardId) {
+    var winning = cards[cardId].w;
+    var chances = cards[cardId].c;
+    var matches = 0;
+    chances.forEach(c => {
+        if (winning.includes(c)) {
+            matches++;
+        }
+    })
+    return matches;
+    // if (matches > 0) {
+    //     return Math.pow(2, matches-1);
+    // }
+    // return 0;
+}
+
+var stack = [];
+var won = 0;
+
+input.forEach(i => {
+    var bits = /Card\s+(\d+):(.+)\|(.+)/.exec(i);
+    var winning = Object.values(bits[2].trim().split(' ').filter(f => f != '').reduce((a, c) => { a[c.trim()] = c.trim(); return a; }, {}));
+    var chances = Object.values(bits[3].trim().split(' ').filter(f => f != '').reduce((a, c) => { a[c.trim()] = c.trim(); return a; }, {}));
+    var id = bits[1];
+    cards[id] = {
+        w: winning, 
+        c: chances
+    }
+    stack.push(id);
+    won++;
+});
+
+while(stack.length) {
+    var top = stack.pop();
+    var s = scoreCard(top);
+    var n = parseInt(top);
+    for(var x = 1; x <= s; x++) {
+        won++;
+        stack.push(''+(n+x));
+    }
+}
+
+console.log(won);
