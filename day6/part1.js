864d142643a0319a99bc5ae301c29bba0f992224
Alex <alexander.ogier@gmail.com>
2023-12-06 20:06:47 +1300

    day6

--- day6/part1.js
+++ day6/part1.js
@@ -1,3 +1,24 @@
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var times = [...input[0].matchAll(/\d+/g)].map(m => parseInt(m[0]));
+var distances = [...input[1].matchAll(/\d+/g)].map(m => parseInt(m[0]));
+
+var ans = 1;
+
+for(var r = 0; r < times.length; r++) {
+    var sum = 0;
+    var totalTime = times[r];
+
+    for(var t = 1; t < totalTime; t++) {
+        var dist = t * (totalTime - t);
+        if (dist > distances[r]) {
+            sum++;
+        }
+    }
+
+    ans *= sum;
+}
+
+console.log(ans);
--- day6/part2.js
+++ day6/part2.js
@@ -1,3 +1,18 @@
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var time = parseInt([...input[0].matchAll(/\d+/g)].map(m => (m[0])).reduce((a, c) => a+c, ''));
+var distance = parseInt([...input[1].matchAll(/\d+/g)].map(m => (m[0])).reduce((a, c) => a+c, ''));
+
+var sum = 0;
+var totalTime = time;
+
+for(var t = 1; t < totalTime; t++) {
+    var dist = t * (totalTime - t);
+    if (dist > distance) {
+        sum++;
+    }
+}
+
+console.log(sum);
