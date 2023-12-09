59d8420cae7826f1554b9e9d8778675b495ba6c8
Alex <alexander.ogier@gmail.com>
2023-12-09 18:18:11 +1300

    day9

--- day9/part1.js
+++ day9/part1.js
@@ -1,3 +1,25 @@
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var sequences = input.map(i => i.split(' ').map(n => parseInt(n)));
+
+var sum = 0;
+
+sequences.forEach(s => {
+    var stack = [s];
+    while(!stack[stack.length-1].every(v => v == 0)) {
+        var newSeq = [];
+        for(var i = 0; i < stack[stack.length-1].length - 1; i++) {
+            newSeq.push(stack[stack.length-1][i+1] - stack[stack.length-1][i]);
+        }
+        stack.push(newSeq);
+    }
+    var inc = 0;
+    while(stack.length) {
+        inc += stack.pop().pop();
+    }
+    sum += inc;
+});
+
+console.log(sum);
--- day9/part2.js
+++ day9/part2.js
@@ -1,3 +1,26 @@
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var sequences = input.map(i => i.split(' ').map(n => parseInt(n)));
+
+var sum = 0;
+
+sequences.forEach(s => {
+    var stack = [s];
+    while(!stack[stack.length-1].every(v => v == 0)) {
+        var newSeq = [];
+        for(var i = 0; i < stack[stack.length-1].length - 1; i++) {
+            newSeq.push(stack[stack.length-1][i] - stack[stack.length-1][i+1]);
+        }
+        stack.push(newSeq);
+    }
+    var inc = 0;
+    while(stack.length) {
+        var p = stack.pop(); 
+        inc += p.shift();
+    }
+    sum += inc;
+});
+
+console.log(sum);
