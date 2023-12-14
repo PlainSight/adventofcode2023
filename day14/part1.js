11b350cb4f137face7e48b486c906ea6fe0e8ed0
Alex <alexander.ogier@gmail.com>
2023-12-14 19:52:41 +1300

    day14

--- day14/part1.js
+++ day14/part1.js
@@ -1,3 +1,50 @@
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var blockers = [];
+var rollers = [];
+
+input.forEach((i, y) => {
+    i.split('').forEach((j, x) => {
+        if (j == '#') {
+            blockers.push({
+                x: x,
+                y: y
+            });
+        }
+        if (j == 'O') {
+            rollers.push({
+                x: x,
+                y: y
+            });
+        }
+    });
+});
+
+function unblocked(x, y) {
+    if (x < 0 || y < 0) {
+        return false;
+    }
+    return blockers.filter(b => b.x == x && b.y == y).length == 0 && rollers.filter(r => r.x == x && r.y == y).length == 0;
+}
+
+var movement = true;
+
+while(movement) {
+    movement = false;
+    rollers.forEach(r => {
+        if (unblocked(r.x, r.y-1)) {
+            r.y--;
+            movement = true;
+        }
+    })
+}
+
+var len = input.length;
+
+var sum = rollers.reduce((a, c) => {
+    return a + (len - c.y)
+}, 0);
+
+console.log(sum);
--- day14/part2.js
+++ day14/part2.js
@@ -1,3 +1,94 @@
 var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var blockers = [];
+var rollers = [];
+
+var maxx = input[0].length;
+var maxy = input.length;
+
+input.forEach((i, y) => {
+    var line = [];
+    i.split('').forEach((j, x) => {
+        line.push(j);
+        if (j == 'O') {
+            rollers.push({
+                x: x,
+                y: y
+            });
+        }
+    });
+    blockers.push(line);
+});
+
+var cycleByScore = {};
+var scoreByCycle = {};
+var hashesByCycle = {};
+var cycleByHash = {};
+
+var ds = [
+    [0, -1],
+    [-1, 0],
+    [0, 1],
+    [1, 0]
+];
+
+function hash(rollers) {
+    return rollers.sort((a, b) => (a.y*maxx + a.x) - (b.y*maxx + b.x)).reduce((a, c) => a+':'+c.x+','+c.y, '');
+}
+
+function unblocked(x, y) {
+    if (blockers[y] && blockers[y][x] == '.') {
+        return true;
+    }
+    return false;
+}
+
+
+for(var c = 1; c < 1000000000; c++) {
+    for(var d = 0; d < 4; d++) {
+        var movement = true;
+
+        while(movement) {
+            movement = false;
+            rollers.forEach(r => {
+                while (unblocked(r.x+ds[d][0], r.y+ds[d][1])) {
+                    blockers[r.y][r.x] = '.';
+                    r.x += ds[d][0];
+                    r.y += ds[d][1];
+                    blockers[r.y][r.x] = 'O';
+                    movement = true;
+                }
+            })
+        }
+    }
+
+    var sum = Object.values(rollers).reduce((a, c) => {
+        return a + (maxy - c.y)
+    }, 0);
+
+    var h = hash(rollers);
+
+    if (cycleByHash[h]) {
+        var last = cycleByHash[h];
+        var diff = c - last;
+
+        // find number of iterations from last to get to 1000000000, then find offset from 1000000000
+        var toIter = 1000000000 - last;
+        var fullLoops = Math.floor(toIter / diff);
+        var offset = toIter - fullLoops*diff;
+        var cycleWithfinalScore = last + offset;
+        console.log(scoreByCycle[cycleWithfinalScore]);
+        return;
+    } else {
+        scoreByCycle[c] = sum;
+        hashesByCycle[c] = hash(rollers);
+        cycleByHash[h] = c;
+
+        cycleByScore[sum] = (cycleByScore[sum] || []);
+        cycleByScore[sum].push(c);
+    }
+}
+
+
