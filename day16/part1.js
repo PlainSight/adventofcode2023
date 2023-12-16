3d0728f956980b9d0d99b186fe3d54800ce6f366
Alex <alexander.ogier@gmail.com>
2023-12-17 01:32:48 +1300

    day16

--- day16/part1.js
+++ day16/part1.js
@@ -1,3 +1,95 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(l => l.split(''));
+
+var seen = {};
+var uniqueXY = {};
+var stack = [{
+    x: 0,
+    y: 0,
+    dx: 1,
+    dy: 0
+}];
+
+function nextDirs(ds, char) {
+    switch (char) {
+        case '.':
+            return [ds];
+        case '-':
+            if (ds[0] != 0) {
+                return [ds];
+            } else {
+                return [[-1, 0], [1, 0]];
+            }
+        case '|':
+            if (ds[1] != 0) {
+                return [ds];
+            } else {
+                return [[0, -1], [0, 1]];
+            }
+        case '/':
+            if (ds[0] == 1) {
+                return [[0, -1]];
+            } else {
+                if (ds[0] == -1) {
+                    return [[0, 1]];
+                } else {
+                    if (ds[1] == 1) {
+                        return [[-1, 0]];
+                    } else {
+                        return [[1, 0]];
+                    }
+                }
+            }
+        case '\\':
+            if (ds[0] == 1) {
+                return [[0, 1]];
+            } else {
+                if (ds[0] == -1) {
+                    return [[0, -1]];
+                } else {
+                    if (ds[1] == 1) {
+                        return [[1, 0]];
+                    } else {
+                        return [[-1, 0]];
+                    }
+                }
+            }
+    }
+}
+
+function key(n) {
+    return [n.x, n.y, n.dx, n.dy].join(',');
+}
+
+while(stack.length) {
+    var top = stack.pop();
+
+    if (seen[key(top)]) {
+        continue;
+    } else {
+        seen[key(top)] = true;
+    }
+
+    if (top.x >= input[0].length || top.x < 0) { 
+        continue;
+    }
+    if (top.y >= input.length || top.y < 0) { 
+        continue;
+    }
+
+    uniqueXY[top.x+','+top.y] = true;
+
+    var nexts = nextDirs([top.dx, top.dy], input[top.y][top.x]);
+
+    nexts.forEach(n => {
+        stack.push({
+            x: top.x+n[0],
+            y: top.y+n[1],
+            dx: n[0],
+            dy: n[1]
+        })
+    })
+}
+
+console.log(Object.values(uniqueXY).length);
--- day16/part2.js
+++ day16/part2.js
@@ -1,3 +1,155 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(l => l.split(''));
+
+function nextDirs(ds, char) {
+    switch (char) {
+        case '.':
+            return [ds];
+        case '-':
+            if (ds[0] != 0) {
+                return [ds];
+            } else {
+                return [[-1, 0], [1, 0]];
+            }
+        case '|':
+            if (ds[1] != 0) {
+                return [ds];
+            } else {
+                return [[0, -1], [0, 1]];
+            }
+        case '/':
+            if (ds[0] == 1) {
+                return [[0, -1]];
+            } else {
+                if (ds[0] == -1) {
+                    return [[0, 1]];
+                } else {
+                    if (ds[1] == 1) {
+                        return [[-1, 0]];
+                    } else {
+                        return [[1, 0]];
+                    }
+                }
+            }
+        case '\\':
+            if (ds[0] == 1) {
+                return [[0, 1]];
+            } else {
+                if (ds[0] == -1) {
+                    return [[0, -1]];
+                } else {
+                    if (ds[1] == 1) {
+                        return [[1, 0]];
+                    } else {
+                        return [[-1, 0]];
+                    }
+                }
+            }
+    }
+}
+
+function key(n) {
+    return [n.x, n.y, n.dx, n.dy].join(',');
+}
+
+var max = 0;
+
+var dirs = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1]
+]
+
+for(var d = 0; d < dirs.length; d++) {
+    var sx = 0;
+    var sy = 0;
+    var sdx = 0;
+    var sdx = 0;
+    var smx = 0;
+    var smy = 0;
+    if (dirs[d][0] != 0) {
+        if (dirs[d][0] == 1) {
+            sx = 0;
+            sy = 0;
+            smx = 0;
+            smy = input.length - 1;
+            sdx = 1;
+            sdy = 0;
+        } else {
+            sx = input[0].length - 1;
+            sy = 0;
+            smx = input[0].length- 1;
+            smy = input.length - 1;
+            sdx = -1;
+            sdy = 0;
+        }
+    } else {
+        if (dirs[d][1] == 1) {
+            sx = 0;
+            sy = 0;
+            smx = input[0].length - 1;
+            smy = 0;
+            sdx = 0;
+            sdy = 1;
+        } else {
+            sx = 0;
+            sy = input.length - 1;
+            smx = input[0].length - 1;
+            smy = input.length - 1;
+            sdx = 0;
+            sdy = -1;
+        }
+    }
+
+    for(var i = Math.min(sx, sy); i <= Math.max(smx, smy); i++) {
+        var xx = sx == smx ? sx : sx + i;
+        var yy = sy == smy ? sy : sy + i;
+
+        var seen = {};
+        var uniqueXY = {};
+    
+        var stack = [{
+            x: xx,
+            y:yy,
+            dx: sdx,
+            dy: sdy
+        }];
+        
+        while(stack.length) {
+            var top = stack.pop();
+        
+            if (seen[key(top)]) {
+                continue;
+            } else {
+                seen[key(top)] = true;
+            }
+        
+            if (top.x >= input[0].length || top.x < 0) { 
+                continue;
+            }
+            if (top.y >= input.length || top.y < 0) { 
+                continue;
+            }
+        
+            uniqueXY[top.x+','+top.y] = true;
+        
+            var nexts = nextDirs([top.dx, top.dy], input[top.y][top.x]);
+        
+            nexts.forEach(n => {
+                stack.push({
+                    x: top.x+n[0],
+                    y: top.y+n[1],
+                    dx: n[0],
+                    dy: n[1]
+                })
+            })
+        }
+        
+        max = max < Object.values(uniqueXY).length ? Object.values(uniqueXY).length : max;
+    }
+}
+
+console.log(max);
+
