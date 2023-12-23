1db92b657ac11c69deb0719d20a4bfce09c26feb
Alexander Ogier <alexander.ogier@gmail.com>
2023-12-23 23:32:35 +1300

    optimise part 2, still quite slow though

--- day23/part2.js
+++ day23/part2.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-const { start } = require('repl');
 
 var world = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(l => l.split(''));
 
@@ -26,8 +25,12 @@ chokepointsCount++;
 chokepoints[endX+','+endY] = chokepointsCount;
 chokepointsCount++;
 
+
 for(var y = 1; y < world.length-1; y++) {
     for(var x = 0; x < world[0].length; x++) {
+        if (world[y][x] == '#') {
+            continue;
+        }
         var openDirections = 0;
         dirs.forEach(d => {
             var nx = x + d[0];
@@ -50,25 +53,22 @@ Object.entries(chokepoints).forEach(e => {
     var xy = e[0].split(',').map(n => parseInt(n));
     var v = parseInt(e[1]);
 
-    var estack = [[xy[0], xy[1], 0]];
-    var seen = {};
+    var estack = [[xy[0], xy[1], -1, -1, 0]];
 
     while(estack.length) {
-        var [x, y, dist] = estack.pop();
+        var [x, y, px, py, dist] = estack.pop();
         dirs.forEach(d => {
             var nx = x + d[0];
             var ny = y + d[1];
 
-            if (ny > 0 && ny < world.length && (world[ny][nx] != '#') && !seen[nx+','+ny]) {
+            if (ny >= 0 && ny < world.length && (nx != px || ny != py) && world[ny][nx] != '#') {
                 var choke = chokepoints[nx+','+ny];
                 if (choke) {
                     destChokes.push([choke, dist+1]);
                 } else {
-                    estack.push([nx, ny, dist+1]);
+                    estack.push([nx, ny, x, y, dist+1]);
                 }
             }
-
-            seen[nx+','+ny] = true;
         })
     }
     edgesFrom[v] = destChokes;
@@ -78,12 +78,28 @@ var longestPath = 0;
 
 var stack = [];
 
-stack.push(1);
+stack.push([1, -1, 0, 2, 0]);
 
-var bestDist = {}; // key = start,end
+var bestDist = {}; // key = start,[ordered middle],end
+
+function addSeen(seen, seen2, next) {
+    if (next > 25) {
+        return [seen, seen2 | (1 << (next - 25))];
+    } else {
+        return [seen | (1 << next), seen2];
+    }
+}
+
+function hasSeen(seen, seen2, choke) {
+    if (choke > 25) {
+        return (seen2 & (1 << (choke - 25))) != 0;
+    } else {
+        return (seen & (1 << choke)) != 0;
+    }
+}
 
 while(stack.length) {
-    var [c, p, dist, seenchoke] = stack.pop();
+    var [c, p, dist, seen, seen2] = stack.pop();
 
     if (c == 2) {
         if (dist > longestPath) {
@@ -91,36 +107,24 @@ while(stack.length) {
         }
         continue;
     }
-    edgesFrom[c].forEach(to => {
-        if (to[])
-    })
 
-    dirs.forEach(d => {
-        var nx = x + d[0];
-        var ny = y + d[1];
+    var edgesToFollow = edgesFrom[c].filter(e => e[0] != p);
+    if (edgesToFollow.some(e => e[0] == 2)) {
+        edgesToFollow = edgesToFollow.filter(e => e[0] == 2);
+    }
+    edgesToFollow.forEach(to => {
+        if (!hasSeen(seen, seen2, to[0])) {
+            var [newSeen, newSeen2] = addSeen(seen, seen2, to[0]);
 
-        var choke = chokepoints[nx+','+ny];
+            var key = `${newSeen},${newSeen2},${to[0]}`;
 
-        if ((nx != px || ny != py) && ny >= 0 && (world[ny][nx] != '#') && (!choke || !seenchoke.some(c => c == choke))) {
-            var nsc = [...seenchoke];
-            if (choke) {
-                nsc.push(choke);
-
-                var startkey = nsc[0];
-                var midkey = nsc.slice(1, -2).sort((a, b) => b-a).join(',');
-                var endkey = nsc[nsc.length-1];
-                var key = `${startkey},${midkey},${endkey}`;
-
-                var bd = bestDist[key];
-                if (!bd || dist+1 > bd) {
-                    bestDist[key] = dist+1;
-                    stack.push([nx, ny, x, y, dist+1, nsc]);
-                }
-            } else {
-                stack.push([nx, ny, x, y, dist+1, nsc]);
+            var bd = bestDist[key];
+            if (!bd || (dist+to[1]) > bd) {
+                bestDist[key] = (dist+to[1]);
+                stack.push([to[0], c, dist+to[1], newSeen, newSeen2]);
             }
         }
     })
 }
 
-console.log('finished', longestPath);
+console.log(longestPath);
